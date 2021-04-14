62635073b39e0007b861d5e0ea2296f53d0c9e01
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-04-14 17:41:39 +0900

    removed build/node_modules

--- build/controller/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// class UserController implements IController {
-//
-// }
-//# sourceMappingURL=user.js.map
--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var app_1 = __importDefault(require("./app"));
-var port = 3000;
-app_1["default"].listen(port);
-//# sourceMappingURL=index.js.map
