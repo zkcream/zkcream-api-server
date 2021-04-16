77c63f3146332649a8a1ed5c70ca4bdc5381082c
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-04-16 10:48:48 +0900

    typo

--- ts/middlewares/errors.ts
+++ ts/middlewares/errors.ts
@@ -4,7 +4,7 @@ export = async (ctx: Koa.Context, next) => {
     try {
         await next()
     } catch (e) {
-        ctx.thorw(e)
+        ctx.throw(e)
         console.log(e.message, e.stack)
     }
 }
