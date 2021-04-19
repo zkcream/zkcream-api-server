ce24a626c813889bdd39e70a6fdcd30fb2d1303a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-04-19 12:33:27 +0900

    Update github action code

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   testPathIgnorePatterns: [
+    '/cream',
     '/build/',
     'node_modules'
   ],
-  testRegex:'/__tests__/.*\\.test\\.ts$',
+  testRegex:'./ts/__tests__/.*\\.test\\.ts$',
   testEnvironment: 'node',
 }
