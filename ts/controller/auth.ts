45ac89895ac7e892e424b7795573a057dd2c0f58
Kazuhiro Nakagawa <knman7@hotmail.com>
2021-10-08 11:25:59 +0900

    Test auth using echo

--- ts/controller/auth.ts
+++ ts/controller/auth.ts
@@ -5,12 +5,12 @@ import '../middlewares/passporthandler'
 
 export class AuthController {
   public authenticate(ctx: Koa.Context, next: Koa.Next) {
-    return passport.authenticate('jwt', function (err, user, info) {
+    return passport.authenticate('jwt', function (err, user) {
       if (!user) {
         ctx.throw(401, 'unauthorized')
       } else {
-        return next()
+        next()
       }
-    })
+    })(ctx, next)
   }
 }
