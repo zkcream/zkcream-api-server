a1cd15442168b9358e2b6e450d75237f1b8bfda1
Kazuhiro Nakagawa <knman7@hotmail.com>
2021-10-11 14:17:25 +0900

    Fix auth controller

--- ts/controller/auth.ts
+++ ts/controller/auth.ts
@@ -1,16 +1,18 @@
 import Koa from 'koa'
 
 import passport from 'koa-passport'
-import '../middlewares/passporthandler'
+import '../middlewares/passport'
 
-export class AuthController {
-  public authenticate(ctx: Koa.Context, next: Koa.Next) {
-    return passport.authenticate('jwt', function (err, user) {
+class AuthController {
+  public async authenticate(ctx: Koa.Context, next: Koa.Next) {
+    await passport.authenticate('jwt', function (err, user) {
       if (!user) {
         ctx.throw(401, 'unauthorized')
       } else {
-        next()
+        return next()
       }
     })(ctx, next)
   }
 }
+
+export const jwtauth = new AuthController().authenticate
