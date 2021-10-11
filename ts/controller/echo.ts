338de54df325c3dffab94e7c04de7ff48612c12b
Kazuhiro Nakagawa <knman7@hotmail.com>
2021-10-11 14:20:17 +0900

    Remove jwt auth for echo

--- ts/controller/echo.ts
+++ ts/controller/echo.ts
@@ -2,20 +2,14 @@ import Router from 'koa-router'
 import Koa from 'koa'
 
 import { IController } from './interface'
-import { AuthController } from './auth'
 
 class EchoController implements IController {
   private Router = new Router()
-  private authController: AuthController = new AuthController()
 
   constructor() {}
 
   public router = (): Router => {
-    return this.Router.get(
-      '/:msg',
-      this.authController.authenticate,
-      this.echo.bind(this)
-    )
+    return this.Router.get('/:msg', this.echo.bind(this))
   }
 
   private echo = async (ctx: Koa.Context) => {
