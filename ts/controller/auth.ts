c38b91aade64da3c20517d2a2e05318aeece6894
Kazuhiro Nakagawa <knman7@hotmail.com>
2021-10-28 16:33:07 +0900

    Remove user auth

--- ts/controller/auth.ts
+++ ts/controller/auth.ts
@@ -2,7 +2,6 @@ import Koa from 'koa'
 
 import passport from 'koa-passport'
 import '../middlewares/passport'
-import { userType } from '../model/user'
 
 class AuthController {
   public async authenticate(ctx: Koa.Context, next: Koa.Next) {
@@ -14,20 +13,6 @@ class AuthController {
       }
     })(ctx, next)
   }
-
-  public async authenticateAdmin(ctx: Koa.Context, next: Koa.Next) {
-    await passport.authenticate('jwt', function (err, user) {
-      if (process.env.NODE_ENV === 'test') {
-        return next()
-      }
-      if (user && user.role == userType.ADMIN) {
-        return next()
-      } else {
-        ctx.throw(401, 'unauthorized')
-      }
-    })(ctx, next)
-  }
 }
 
 export const jwtauth = new AuthController().authenticate
-export const adminauth = new AuthController().authenticateAdmin
