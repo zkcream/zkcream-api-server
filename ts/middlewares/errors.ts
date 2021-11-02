88622bb218bf563e8f66d774942606dbffa2a013
Kazuhiro Nakagawa <knman7@hotmail.com>
2021-11-02 15:29:04 +0900

    Limit access to faucet by IP and wallet address

--- ts/middlewares/errors.ts
+++ ts/middlewares/errors.ts
@@ -8,6 +8,7 @@ export = async (ctx: Koa.Context, next) => {
         message: e.message,
       }
     } else {
+      console.log(e)
       ctx.status = 500
       ctx.body = {
         message: 'Internal Server Error',
